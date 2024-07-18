6019b4268b2ea7b567bd2ffc0eb591259eb0f248
gae <gaedev22@gmail.com>
2024-07-18 16:22:30 +0900

    update

--- install.sh
+++ install.sh
@@ -32,15 +32,15 @@ if [ ! -d "$HOME/.oh-my-zsh" ]; then
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi
 
-if [ ! -d "$HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions" ]; then
-    echo "Installing zsh-autosuggestions..."
-    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-fi
-
 # powerlevel10kのインストール
 echo "Installing Powerlevel10k..."
 if [! -d "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k" ]; then
     git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+fi
+
+if [ ! -d "$HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions" ]; then
+    echo "Installing zsh-autosuggestions..."
+    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 fi
 
 if [ ! -d "$HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting" ]; then
@@ -65,4 +65,4 @@ fi
 ln -sf $HOME/.zsh-minimum/zshrc.zsh $HOME/.zshrc
 ln -sf $HOME/.zsh-minimum/vimrc.vim $HOME/.vimrc
 
-echo "Installation complete. Please restart your terminal."
+source $HOME/.zshrc
